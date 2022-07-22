ef6f99f3d43da8f62b5ade827521aeea482f2e17
KDilara <106311483+KDilara@users.noreply.github.com>
2022-07-22 20:46:09 +0400

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -1,11 +1,17 @@
 let children = [
-    {title:'Aytac', weight: 22}, 
-    {title:'Aynur', weight: 33}, 
-    {title:'Murad', weight: 30}, 
-    {title:'Dadaş', weight: 28}, 
-    {title:'Seymur', weight: 25}, 
-    {title:'Leyla', weight: 42}, 
-    {title:'Lalə', weight: 35}, 
-    {title:'Samir', weight: 39}, 
+  {title:'Aytac', weight: 22}, 
+  {title:'Aynur', weight: 33}, 
+  {title:'Murad', weight: 30}, 
+  {title:'Dadaş', weight: 28}, 
+  {title:'Seymur', weight: 25}, 
+  {title:'Leyla', weight: 42}, 
+  {title:'Lalə', weight: 35}, 
+  {title:'Samir', weight: 39}, 
 ]
-    // Write code here
+let maxAge = -Infinity;
+for(obj of children){
+  if(obj.weight > maxAge){
+    maxAge = obj.weight;
+  }
+}
+console.log(maxAge);
